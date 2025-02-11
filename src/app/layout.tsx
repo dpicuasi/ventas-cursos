50eedf82549acf2107c570afac277b008ae23d0d
dpicuasi <danyeledg@gmail.com>
2025-02-11 14:52:48 -0500

    Prepara para deployar

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Bricolage_Grotesque } from "next/font/google";
+import "./globals.css";
+import type React from "react"; // Import React
+
+const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "ShipFree",
+  description:
+    "Open Source Alternative to ShipFast - Launch your startup in days Not in weeks",
+  keywords: [
+    "saas",
+    "boilerplate",
+    "open source",
+    "free",
+    "open source shipfast",
+    "shipfree",
+    "idee8",
+    "made by idee8",
+    "free boilerplate",
+    "github",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={bricolageGrotesque.className}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
